571b957959c2f3f8342632b86ea4ba999b8d4a55
xlfrrez <alfrrezib24@gamil.com>
2026-05-29 20:26:36 -0600

    primera prueba

---
+++ libro.py
@@ -0,0 +1,11 @@
+class Libro:
+    def __init__(self, titulo, autor, genero, año):
+        self.titulo=titulo
+        self.autor=autor
+        self.genero=genero
+        self.año=año
+        self.estado="disponible"
+
+    def mostrar(self):
+         print(f"{self.titulo}  {self.autor}  {self.genero}  {self.año}  {self.estado}")
+         
