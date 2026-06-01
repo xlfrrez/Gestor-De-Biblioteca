8e10a58ca1f4ee8b12f7c74f1c0e13ab71c3ee6c
Jorgeyunior05 <jorgeyunior93@gmail.com>
2026-06-01 08:56:29 -0600

    validar usuario y libros
    este archivo es para validar el usuario y validar libros si esque se encuentran o no

---
+++ validaciones.py
@@ -0,0 +1,11 @@
+def validar_usuario(usuario):
+    if usuario is None:
+        print("El nombre de usuario no se encuentra")
+        return False
+    return True
+
+def validar_libro(libro):
+    if libro is None:
+        print("El libro no encontrado")
+        return False
+    return True
