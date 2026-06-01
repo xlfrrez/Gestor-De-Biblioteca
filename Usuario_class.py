8f725ecdc9a2050bcaa8dc8faad4daa4859c6a53
enriquefloflo1506-code <enriquefloflo1506@gmail.com>
2026-06-01 08:57:24 -0600

    Create Usuario_class.py

---
+++ Usuario_class.py
@@ -0,0 +1,8 @@
+def_init_(self,nombre,id_usuario):
+  self.nombre=nombre
+  self.id_usuario
+  self.libros_prestados=[]
+  self.historial[]
+
+def puede_prestar(self):
+  return len(self.libros_prestados)<3
