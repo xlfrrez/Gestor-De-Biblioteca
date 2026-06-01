74bfe606b9748ddc788a197410b623bd1dbf86fd
Jorgeyunior05 <jorgeyunior93@gmail.com>
2026-06-01 09:21:03 -0600

    Eliminar libros y usuarios
    este archivo elimina a los usuarios y libros ya no funcionales

---
+++ FUNCIONES/Eliminar_libro.py
@@ -0,0 +1,15 @@
+def eliminar_libro(bibloteca, titulo):
+  for l in bibloteca.libros:
+    if if l.titulo.lower() == titulo.lower():
+      biblioteca.libros.remove(l)
+      print("Libro eliminado")
+      return
+print("No encontrado")
+
+def eliminar_usuario(biblioteca, nombre):
+    for u in biblioteca.usuarios:
+        if u.nombre.lower() == nombre.lower():
+            biblioteca.usuarios.remove(u)
+            print("Usuario eliminado")
+            return
+    print("No encontrado")
