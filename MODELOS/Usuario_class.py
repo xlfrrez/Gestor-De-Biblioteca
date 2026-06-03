664442ab10e2cce865d64e485adb3a69a81868e7
xlfrrez <alfrrezib24@gmail.com>
2026-06-02 21:00:41 -0600

    Update Usuario_class.py
    actualizacion

--- MODELOS/Usuario_class.py
+++ MODELOS/Usuario_class.py
@@ -1,8 +1,9 @@
-def_init_(self,nombre,id_usuario):
-  self.nombre=nombre
-  self.id_usuario
-  self.libros_prestados=[]
-  self.historial[]
-
-def puede_prestar(self):
-  return len(self.libros_prestados)<3
+class Usuario:
+  def __init__(self, nombre, id_usuario):
+    self.nombre = nombre
+    self.id_usuario = id_usuario
+    self.libros_prestados = []
+    self.historial = []
+    
+  def puede_prestar(self):
+    return len(self.libros_prestados) < 3
