d4a35aa2a7a2bad17b79311bf39f1863f828d5c2
xlfrrez <alfrrezib24@gmail.com>
2026-06-04 08:56:49 -0600

    Refactor menu options and update import statement
    menu actualizado de gestor de biblioteca

--- FUNCIONES/menu.py
+++ FUNCIONES/menu.py
@@ -6,7 +6,7 @@ from funciones.reportes import *
 from funciones.validaciones import *
 from funciones.historial import *
 from funciones.estadisticas import *
-from funciones.eliminar import *
+from funciones.Eliminar import *
 
 
 def menu():
@@ -32,71 +32,71 @@ def menu():
     match opcion:
 
     
-    case "1":
-      biblioteca.agregar_libro(
-        Libro(input("Titulo: "), input("Genero: "), input("Año: "))
-      )
+     case "1":
+        biblioteca.agregar_libro(
+          Libro(input("Titulo: "), input("Autor: "), input("Genero: "), input("Año: "))
+        )
 
     
-    case "2":
-      biblioteca.registrar_usuario(
-        Usuario(input("Nombre: "), input("Autor: "), input("Genero: "), input("Año: "))
-      )
+     case "2":
+        biblioteca.registrar_usuario(
+          Usuario(input("Nombre: "), input("ID: "))
+        )
 
     
-    case "3":
-      t = input("Libro: ")
-      u = input("Usuario: ")
+     case "3":
+        t = input("Libro: ")
+        u = input("Usuario: ")
       
-      usuario = next((x for x in biblioteca.usuarios if x.nombre == u), None)
-      libro = next((x for x in biblioteca.libros if x.titulo == t), None)
-
-      
-      if usuario and libro:
-        biblioteca.prestar(libro, usuario)
-
-    
-    case "4":
-      t = input("Libro: ")
-      u = input("Usuario: ")
+        usuario = next((x for x in biblioteca.usuarios if x.nombre == u), None)
+        libro = next((x for x in biblioteca.libros if x.titulo == t), None)
 
       
-      usuario = next((x for x in biblioteca.usuarios if x.nombre == u), None)
-      libro = next((x for x in biblioteca.libros if x.titulo == t), None)
+        if usuario and libro:
+          biblioteca.prestar(libro, usuario)
+
+    
+     case "4":
+        t = input("Libro: ")
+        u = input("Usuario: ")
 
       
-      if usuario and libro:
-        biblioteca.devolver(libro, usuario)
+        usuario = next((x for x in biblioteca.usuarios if x.nombre == u), None)
+        libro = next((x for x in biblioteca.libros if x.titulo == t), None)
+
+      
+        if usuario and libro:
+          biblioteca.devolver(libro, usuario)
 
     
-    case "5":
-      texto = input("Buscar: ")
-      for l in biblioteca.buscar(texto):
-        l.mostrar()
+     case "5":
+        texto = input("Buscar: ")
+        for l in biblioteca.buscar(texto):
+          l.mostrar()
 
     
-    case "6":
-      libros_disponibles(biblioteca)
+     case "6":
+        libros_disponibles(biblioteca)
 
     
-    case "7":
-      estadisticas(biblioteca)
+     case "7":
+        estadisticas(biblioteca)
 
     
-    case "8":
-      nombre = input("Usuario: ")
-      usuario = next((x for x in biblioteca.usuarios if x.nombre == nombre), None)
-      if usuario:
-        ver_historial(usuario)
+     case "8":
+        nombre = input("Usuario: ")
+        usuario = next((x for x in biblioteca.usuarios if x.nombre == nombre), None)
+        if usuario:
+          ver_historial(usuario)
 
     
-    case "9":
-      eliminar_libro(biblioteca, input("Tutulo: "))
+     case "9":
+        eliminar_libro(biblioteca, input("Tutulo: "))
 
     
-    case "10":
-      eliminar_usuario(biblioteca, input("Nombre: "))
+     case "10":
+        eliminar_usuario(biblioteca, input("Nombre: "))
 
     
-    case "11":
-      break
+     case "11":
+        break
