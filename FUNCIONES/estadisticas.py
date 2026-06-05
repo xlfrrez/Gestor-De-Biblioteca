b124cfbac9502d6d75f14b90340fa1294f83d8bb
xlfrrez <alfrrezib24@gmail.com>
2026-06-04 19:49:44 -0600

    Fix indentation for print statements in estadisticas.py
    actualizacion

--- FUNCIONES/estadisticas.py
+++ FUNCIONES/estadisticas.py
@@ -3,7 +3,7 @@ def estadisticas(biblioteca):
   prestados = len([l for l in biblioteca.libros if l.estado == "prestado"])
   disponibles = total - prestados
 
-print("\n Estadisticas")
-print("Total de Libros:", total)
-print("Prestados:", prestados)
-print("Disponibles:", disponibles)
+  print("\n Estadisticas")
+  print("Total de Libros:", total)
+  print("Prestados:", prestados)
+  print("Disponibles:", disponibles)
